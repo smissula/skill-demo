2b55a5b2add90b7fb27e883303dc0ea68949e174
Sri @ Yoda <smissula@ucsd.edu>
2022-02-03 18:31:37 -0800

    Fixed for failing test

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,6 +1,7 @@
 class AreaOfCircle {
     public static double calculate(double radius) {
-        double area = (22.0 / 7) * Math.pow(radius, 2);
+        // double area = (22.0 / 7) * Math.pow(radius, 2);
+        double area = Math.PI * Math.pow(radius, 2);
         return area;
     }
 }
